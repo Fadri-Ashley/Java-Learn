443bde41ee1397ea69792a8fe2d3a7bbef8ea29d
Fadri <fadriasliarga@gmail.com>
2026-09-07 22:27:18 +0700

    create addition operation

---
+++ Calculator/Add.java
@@ -0,0 +1,12 @@
+package Calculator;
+
+public class Add {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5; 
+
+        int sum = a + b;
+
+        System.out.println(a + " + " + b + " = " + sum);
+    }
+}
