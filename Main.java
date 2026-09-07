f6f4a6ba9deb8449341212fa65b396a5ac4716e8
Fadri <fadriasliarga@gmail.com>
2026-09-07 22:19:17 +0700

    start over

--- Main.java
+++ Main.java
@@ -1,11 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-        System.out.print("Enter number: ");
-
-        int num = myObj.nextInt();
-        System.out.println("Number is: " + num);
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
     }
 }
