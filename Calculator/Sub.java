cf965aeabf56db7fe69455f3acb01b3642900e4e
Fadri <fadriasliarga@gmail.com>
2026-09-08 12:56:24 +0700

    add subtraction operation

---
+++ Calculator/Sub.java
@@ -0,0 +1,19 @@
+package Calculator;
+import java.util.Scanner;
+
+public class Sub {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input num1: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Input num2: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 - num2;
+
+        System.out.println(num1 + " - " + num2 + " = " + sum);
+
+        scanner.close();
+    }
+}
