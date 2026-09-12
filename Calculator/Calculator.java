a24c8a6fb4220dcc2965da9d946e47e6c6dd3221
Fadri <fadriasliarga@gmail.com>
2026-09-12 15:48:49 +0700

    add while loop

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,44 +1,50 @@
 package Calculator;
+
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Choose Operation: ");
-        System.out.println("1. Addition");
-        System.out.println("2. Substraction");
-        System.out.println("3. Multiply");
-        System.out.println("4. Division");
-        System.out.print("Input number: ");
-        int numMenu = scanner.nextInt();
+        while (true) {
+            System.out.println("Choose Operation: ");
+            System.out.println("1. Addition");
+            System.out.println("2. Substraction");
+            System.out.println("3. Multiply");
+            System.out.println("4. Division");
+            System.out.print("Input number: ");
+            int numMenu = scanner.nextInt();
 
-        if (numMenu < 5 && numMenu > 0) {
-            System.out.print("Input num1: ");
-            int num1 = scanner.nextInt();
-            System.out.print("Input num2: ");
-            int num2 = scanner.nextInt();
+            if (numMenu < 5 && numMenu > 0) {
+                System.out.print("Input num1: ");
+                int num1 = scanner.nextInt();
+                System.out.print("Input num2: ");
+                int num2 = scanner.nextInt();
 
-            switch (numMenu) {
-                case 1:
-                    System.out.println("Result: " + AddNumber(num1, num2));
-                    break;
-                case 2:
-                    System.out.println("Result: " + SubNumber(num1, num2));
-                    break;
-                case 3:
-                    System.out.println("Result: " + MulNumber(num1, num2));
-                    break;
-                case 4:
-                    System.out.println("Result: " + DivNumber(num1, num2));
-                    break;
-                default:
-                    System.out.println("Banano");
+                switch (numMenu) {
+                    case 1:
+                        System.out.println("Result: " + AddNumber(num1, num2));
+                        break;
+                    case 2:
+                        System.out.println("Result: " + SubNumber(num1, num2));
+                        break;
+                    case 3:
+                        System.out.println("Result: " + MulNumber(num1, num2));
+                        break;
+                    case 4:
+                        System.out.println("Result: " + DivNumber(num1, num2));
+                        break;
+                    default:
+                        System.out.println("Banano");
+                }
+            } else if (numMenu == 5) {
+                System.out.println("Bye");
+                break;
+            } else {
+                System.out.println("Invalid Number");
             }
-        } else {
-            System.out.println("Invalid Number");
         }
-        
+        ;
 
         scanner.close();
     }
@@ -56,6 +62,6 @@ public class Calculator {
     }
 
     public static float DivNumber(float num1, float num2) {
-        return num1/num2;
+        return num1 / num2;
     }
 }
